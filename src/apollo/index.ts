a537d54408d8bdf094219b06da4c0ab923d41d2d
The Aman <urtheaman@gmail.com>
2022-05-16 17:15:11 +0530

    node_env url

--- src/apollo/index.ts
+++ src/apollo/index.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://127.0.0.1:3200/graphql',
+  uri:
+    process.env.NODE_ENV === 'production'
+      ? 'https://connect-urtheaman.herokuapp.com/graphql'
+      : 'http://127.0.0.1:3200/graphql',
   cache: new InMemoryCache(),
 });
 
